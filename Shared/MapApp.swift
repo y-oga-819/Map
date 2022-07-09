4cdf59e795ad429984273362d175c843f2fcb31e
yuki819 <yuki819@mba2018.local>
2022-07-10 00:53:15 +0900

    feature(Map): Init Xcode Project

---
+++ Shared/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Shared
+//
+//  Created by 小笠原佑樹 on 2022/07/10.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
