a8ad97e0acb24454545c6645f9412c99c9166e02
yuki819 <yuki819@mba2018.local>
2022-07-11 01:29:25 +0900

    feature(Map): 東京タワーと言うキーワードでその位置にピンを刺す

--- Shared/MapView.swift
+++ Shared/MapView.swift
@@ -9,17 +9,40 @@ import SwiftUI
 import MapKit
 
 struct MapView: UIViewRepresentable {
+    let searchWord: String
+
     func makeUIView(context: Context) -> MKMapView {
         MKMapView()
     }
-    
+
     func updateUIView(_ uiView: MKMapView, context: Context) {
+        print(searchWord)
         
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(
+            searchWord,
+            completionHandler: { (placemarks, error) in
+            if let unwrapPlacemarks = placemarks,
+               let firstPlacemark = unwrapPlacemarks.first,
+               let location = firstPlacemark.location {
+                let targetCoordinnate = location.coordinate
+                print(targetCoordinnate)
+                
+                let pin = MKPointAnnotation()
+                pin.coordinate = targetCoordinnate
+                pin.title = searchWord
+                uiView.addAnnotation(pin)
+                uiView.region = MKCoordinateRegion(
+                    center: targetCoordinnate,
+                    latitudinalMeters: 500,
+                    longitudinalMeters: 500)
+            }
+        })
     }
 }
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(searchWord: "東京タワー")
     }
 }
